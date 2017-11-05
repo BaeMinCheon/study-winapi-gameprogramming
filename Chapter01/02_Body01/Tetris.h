51254e27a6668914e0db39ae59b01c75d7218eac
baemincheon <baemincheon@gmail.com>
2017-11-05 20:31:18 +0900

    Commit#009

---
+++ Chapter01/02_Body01/Tetris.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Graphics.h"
+
+class Tetris
+{
+private:
+	static BOOL bBlocks[10 * 20];
+	static BOOL bMoves[10 * 20];
+	static BOOL bTemps[10 * 20];
+	static int timeUpdate;
+public:
+	static VOID Draw(VOID);
+	static VOID UpdateBlock(BOOL bForce);
+	static VOID MoveBlocksLeft(VOID);
+	static VOID MoveBlocksRight(VOID);
+	static VOID CreateBlock(VOID);
+};
