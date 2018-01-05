afe00ab33d4042958e8d8756e830e81dd8a75cbf
BaeMinChoen <baemincheon@gmail.com>
2018-01-05 18:13:14 +0900

    v6.0.0

---
+++ Chapter06/01_PixelBlending/System.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Windows.h>
+#include <tchar.h>
+
+int __stdcall _tWinMain(_In_ HINSTANCE p_hInstance,
+	_In_ HINSTANCE p_hPrevInstance,
+	_In_ TCHAR* p_pszLine,
+	_In_ int p_nShow);
